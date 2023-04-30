d537886370dfd9d0837761cf354c06ece14832ab
Titus Soporan <titus@tsoporan.com>
2023-04-30 13:08:30 -0400

    Bump packages + styling
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- pages/_document.js
+++ pages/_document.js
@@ -1,8 +1,6 @@
 import { Head, Html, Main, NextScript } from "next/document";
 
-const linearGradient = `
-linear-gradient(196deg, rgb(255, 255, 255) 0%, rgb(255 255 255) 4%, rgb(252, 252, 252) 15%, rgb(252, 252, 252) 5%, rgb(238 238 238) 26%, rgb(255 255 255) 54%, rgb(218 218 218) 78%, rgb(127 202 126) 113%, rgb(247, 247, 247) 95%, rgb(245, 245, 245) 100%)
-`;
+const linearGradient = `linear-gradient(196deg, rgb(255, 255, 255) 0%, rgb(255 255 255) 4%, rgb(252, 252, 252) 15%, rgb(252, 252, 252) 5%, rgb(255 255 255) 26%, rgb(255 255 255) 54%, rgb(255 255 255) 78%, rgb(127 202 126) 113%, rgb(247, 247, 247) 95%, rgb(245, 245, 245) 100%)`;
 
 const styles = {
   backgroundImage: linearGradient,
