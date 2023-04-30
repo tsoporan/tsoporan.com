d347543561a41a9be0cd26729ddf7a856cc7aaff
Titus Soporan <titus@tsoporan.com>
2023-04-30 13:10:16 -0400

    Add vercel analytics
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import "../styles/globals.css";
 
 import Layout from "../components/layout";
+import { Analytics } from "@vercel/analytics/react";
 
 function App({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   );
 }
